cc6260e8dbd0f5e96e8a444dd4870e1eb7d4d69b
Shreyas Das <dasShreyas2607@outlook.com>
2022-08-13 17:41:45 +0530

    init

--- build_files.sh
+++ build_files.sh
@@ -1,1 +1,4 @@
 pip install -r requirements.txt
+python3.9 manage.py collectstatic
+sudo apt update
+sudo apt install python3-pip python3-dev libpq-dev postgresql postgresql-contrib
--- user_management/settings.py
+++ user_management/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -160,3 +160,5 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
