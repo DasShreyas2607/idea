afd21772958d63f22e0193afdd25f16e6adb3c57
Shreyas Das <dasShreyas2607@outlook.com>
2022-08-13 18:00:46 +0530

    done

--- user_management/settings.py
+++ user_management/settings.py
@@ -85,10 +85,23 @@ WSGI_APPLICATION = 'user_management.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'railway',
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'QGPgs0lkcAqFufy5htj1',
+
+        'HOST': 'containers-us-west-78.railway.app',
+
+        'PORT': '5965',
+
     }
+
 }
 
 
