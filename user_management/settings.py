a32e1581a896000efa258d6509beded6288a8356
Shreyas Das <dasShreyas2607@outlook.com>
2022-08-13 19:05:46 +0530

    o

--- user_management/settings.py
+++ user_management/settings.py
@@ -174,5 +174,5 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
