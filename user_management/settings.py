4a2f98d8031b8ba7576497782fd09c85ebbe1703
Shreyas Das <dasShreyas2607@outlook.com>
2022-08-13 19:33:54 +0530

    o

--- user_management/settings.py
+++ user_management/settings.py
@@ -164,8 +164,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = str(os.getenv('EMAIL_USER'))
-EMAIL_HOST_PASSWORD = str(os.getenv('EMAIL_PASSWORD'))
+EMAIL_HOST_USER = 'runtimet3r@gmail.com'
+EMAIL_HOST_PASSWORD = 'xdnzazmfsnrwxccl'
 
 SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 
